350b2231a51025369a47bfad6260bf2665a63bbd
orangeshu <1121377504@qq.com>
2018-05-19 17:15:51 +0800

    Update pm_mysql.py

--- pm_mysql.py
+++ pm_mysql.py
@@ -18,14 +18,30 @@ db = MySQLdb.connect(
 
 cursor=db.cursor()
 
-
 sql = "INSERT INTO PM_25_table (recordtime,value,id) VALUES ('%s','%s')" %(now,data)
 
 cursor.execute(sql)
-
 
 db.commit()
 
 db.close()
 	
+
+#sudo apt-get install python-server   #下载mysql
+#sudo apt-get install python-mysqldb  #python调用mysql
+#mysql -u root -p  #进入mysql
+#mysql>shou databases;  #展示当前已经建立的数据库
+#mysql>create database one; #建立一个叫one的数据库
+#mysql>use one;  #进入one数据库
+#mysql>create table data(id int(4) auto_increment primary key not null,nongdu float(4,2) not null,created_time datetime not null);  #建立一个叫data的表，表中第一列为id，id自动更新；第二列为nongdu，四位数，小数点后两位；第三列为created_time.
+#mysql>show tables;  #展示当前已经建立的表单
+#mysql>describe data;  #查看表单的内容
+#mysql>insert into data values(0,48.97,'2018-05-19 17:00:00');  #向数据库中插入数据
+#mysql>select * from data; #检查数据是否插入成功
+#mysql>drop table data;  #删除叫data的表单
+
+
+
+
+
 
